1567ca2051eba80402759872f198d15690a6bc09
roycrippen <roycrippen@gmail.com>
2017-12-28 14:17:57 -0600

    pointless syntax highting

---
+++ src/extension.ts
@@ -0,0 +1,24 @@
+import * as vscode from 'vscode';
+
+// Configure in code as wordPattern not supported in JSON format
+const configuration: vscode.LanguageConfiguration = {
+  wordPattern: /\\[^\s]+|[^\\\s\d(){}\[\]#.][^\\\s(){}\[\]#.]*/,
+  "comments": {
+    // symbol used for single line comment. Remove this entry if your language does not support line comments
+    "lineComment": "$",
+    // symbols used for start and end a block comment. Remove this entry if your language does not support block comments
+    "blockComment": ["{", "}"]
+  },
+  // symbols used as brackets
+  "brackets": [
+    ["{", "}"],
+    ["[", "]"],
+    ["(", ")"],
+    ["\"", "\""],
+    ["'", "'"]
+  ]
+};
+
+export function activate() {
+  vscode.languages.setLanguageConfiguration('pointless', configuration);
+}
